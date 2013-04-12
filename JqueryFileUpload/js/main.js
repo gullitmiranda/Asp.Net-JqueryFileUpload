d6c60ce979878b62e92e15883ebf55de358849fc
Gullit Miranda <gullitmiranda@requestdev.com.br>
2013-04-11 23:07:53 -0400

    rest

--- JqueryFileUpload/js/main.js
+++ JqueryFileUpload/js/main.js
@@ -19,7 +19,7 @@ $(function () {
     $('#fileupload').fileupload({
         // Uncomment the following to send cross-domain cookies:
         //xhrFields: {withCredentials: true},
-        // url: 'server/php/'
+        url: $('#fileupload').prop("action")
     });
 
     // Enable iframe cross-domain access via redirect option:
@@ -32,56 +32,50 @@ $(function () {
         )
     );
 
-    // if (window.location.hostname === 'blueimp.github.com' ||
-    //         window.location.hostname === 'blueimp.github.io') {
-        // Demo settings:
-        $('#fileupload').fileupload('option', {
-//            url: '//jquery-file-upload.appspot.com/',
-            maxFileSize: 5000000,
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-            process: [
-                {
-                    action: 'load',
-                    fileTypes: /^image\/(gif|jpeg|png)$/,
-                    maxFileSize: 20000000 // 20MB
-                },
-                {
-                    action: 'resize',
-                    maxWidth: 1440,
-                    maxHeight: 900
-                },
-                {
-                    action: 'save'
-                }
-            ]
+    // settings:
+    $('#fileupload').fileupload('option', {
+        maxFileSize: 5000000,
+        acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+        process: [
+            {
+                action: 'load',
+                fileTypes: /^image\/(gif|jpeg|png)$/,
+                maxFileSize: 20000000 // 20MB
+            },
+            {
+                action: 'resize',
+                maxWidth: 1440,
+                maxHeight: 900
+            },
+            {
+                action: 'save'
+            }
+        ]
+    });
+    // Upload server status check for browsers with CORS support:
+    if ($.support.cors) {
+        $.ajax({
+            // url: '//jquery-file-upload.appspot.com/',
+            type: 'HEAD'
+        }).fail(function () {
+            $('<span class="alert alert-error"/>')
+                .text('Upload server currently unavailable - ' +
+                        new Date())
+                .appendTo('#fileupload');
         });
-        // Upload server status check for browsers with CORS support:
-        if ($.support.cors) {
-            $.ajax({
-                // url: '//jquery-file-upload.appspot.com/',
-                type: 'HEAD'
-            }).fail(function () {
-                $('<span class="alert alert-error"/>')
-                    .text('Upload server currently unavailable - ' +
-                            new Date())
-                    .appendTo('#fileupload');
-            });
-        }
-    // } else {
-    //     // Load existing files:
-    //     $('#fileupload').addClass('fileupload-processing');
-    //     $.ajax({
-    //         // Uncomment the following to send cross-domain cookies:
-    //         //xhrFields: {withCredentials: true},
-    //         url: $('#fileupload').fileupload('option', 'url'),
-    //         dataType: 'json',
-    //         context: $('#fileupload')[0]
-    //     }).always(function (result) {
-    //         $(this).removeClass('fileupload-processing');
-    //     }).done(function (result) {
-    //         $(this).fileupload('option', 'done')
-    //             .call(this, null, {result: result});
-    //     });
-    // }
-
+    }
+    // Load existing files:
+    $('#fileupload').addClass('fileupload-processing');
+    $.ajax({
+        // Uncomment the following to send cross-domain cookies:
+        //xhrFields: {withCredentials: true},
+        url: $('#fileupload').fileupload('option', 'url'),
+        dataType: 'json',
+        context: $('#fileupload')[0]
+    }).always(function (result) {
+        $(this).removeClass('fileupload-processing');
+    }).done(function (result) {
+        $(this).fileupload('option', 'done')
+            .call(this, null, { result: result });
+    });
 });
--- JqueryFileUpload/upload.ashx.cs
+++ JqueryFileUpload/upload.ashx.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Linq;
 using System.Web;
+// using System.Web.Mvc;
 using System.Web.Script.Serialization;
 
 //namespace para manipulação de imagens
@@ -14,65 +17,209 @@ namespace JqueryFileUpload
     /// </summary>
     public class Upload : IHttpHandler
     {
+        private readonly JavaScriptSerializer js;
 
+        // Diretório onde o arquivo será salvo
+        private string StorageRoot { get {
+            return Path.Combine(System.Web.HttpContext.Current.Server.MapPath("~/Uploads/"));
+        } }
+
+        // metodo de inicialização
+        public Upload()
+        {
+            js = new JavaScriptSerializer();
+            js.MaxJsonLength = 41943040;
+        }
+
+        public bool IsReusable { get { return false; } }
+
+        // Processa a requisição
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";//"application/json";
-            var r = new System.Collections.Generic.List<ViewDataUploadFilesResult>();
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            foreach (string file in context.Request.Files)
+            context.Response.AddHeader("Pragma", "no-cache");
+            context.Response.AddHeader("Cache-Control", "private, no-cache");
+
+            // ReturnOptions(context);
+            HandleMethod(context);
+        }
+
+        // Lidar com pedido baseado no método
+        private void HandleMethod(HttpContext context)
+        {
+            switch (context.Request.HttpMethod)
             {
-                HttpPostedFile hpf = context.Request.Files[file] as HttpPostedFile;
-                string FileName = string.Empty;
-                if (HttpContext.Current.Request.Browser.Browser.ToUpper() == "IE")
-                {
-                    string[] files = hpf.FileName.Split(new char[] { '\\' });
-                    FileName = files[files.Length - 1];
-                }
-                else
-                {
-                    FileName = hpf.FileName;
-                }
-                if (hpf.ContentLength == 0)
-                    continue;
+                case "HEAD":
+                case "GET":
+                    if (GivenFilename(context)) DeliverFile(context);
+                    else ListCurrentFiles(context);
+                    break;
 
-                // Carrega stream da imagem do buffer e pega alguns atributos
-                Stream imgStream = context.Request.Files[file].InputStream;
+                case "POST":
+                case "PUT":
+                    UploadFile(context);
+                    break;
 
-                // Define o nome da imagem a ser gravada
-                var imgName = "__" + FileName;
+                case "DELETE":
+                    DeleteFile(context);
+                    break;
 
-                // Localização onde a imagem vai ser salva (relativo a aplicação)
-                var imgPath = "~/imagens";
-                var imgSaveLocation = context.Server.MapPath(imgPath) + "\\" + imgName;
+                case "OPTIONS":
+                    ReturnOptions(context);
+                    break;
 
-                // Tamanho da imagem final
-                int imgHeight = 50;
-                int imgWidth = 50;
-
-                Bitmap newFile = ResizeImage(imgStream, imgWidth, imgHeight);
-                newFile.Save(imgSaveLocation, System.Drawing.Imaging.ImageFormat.Jpeg);
-
-                r.Add(new ViewDataUploadFilesResult()
-                {
-                    thumbnail_url = VirtualPathUtility.ToAbsolute(imgPath) +"/"+ imgName,
-                    name = imgName,
-                    length = hpf.ContentLength,
-                    type = hpf.ContentType
-                });
-                var uploadedFiles = new
-                {
-                    files = r.ToArray()
-                };
-                var jsonObj = js.Serialize(uploadedFiles);
-                //jsonObj.ContentEncoding = System.Text.Encoding.UTF8;
-                //jsonObj.ContentType = "application/json;";
-                context.Response.Write(jsonObj.ToString());
+                default:
+                    context.Response.ClearHeaders();
+                    context.Response.StatusCode = 405;
+                    break;
             }
         }
 
+        // Opções de retorno liberadas
+        private static void ReturnOptions(HttpContext context)
+        {
+            context.Response.AddHeader("Allow", "DELETE,GET,HEAD,POST,PUT,OPTIONS");
+            context.Response.StatusCode = 200;
+        }
+
+        // Remove Arquivo do Servidor
+        private void DeleteFile(HttpContext context)
+        {
+            var filePath = StorageRoot + context.Request["f"];
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+
+        private void UploadFile(HttpContext context)
+        {
+            var statuses = new List<FilesStatus>();
+            var headers = context.Request.Headers;
+
+            if (string.IsNullOrEmpty(headers["X-File-Name"]))
+            {
+                UploadWholeFile(context, statuses);
+            }
+            else
+            {
+                UploadPartialFile(headers["X-File-Name"], context, statuses);
+            }
+
+            WriteJsonIframeSafe(context, statuses);
+        }
+
+        // Enviar arquivo parcial
+        private void UploadPartialFile(string fileName, HttpContext context, List<FilesStatus> statuses)
+        {
+            if (context.Request.Files.Count != 1) throw new HttpRequestValidationException("Attempt to upload chunked file containing more than one fragment per request");
+            var inputStream = context.Request.Files[0].InputStream;
+            var fullName = StorageRoot + Path.GetFileName(fileName);
+
+            using (var fs = new FileStream(fullName, FileMode.Append, FileAccess.Write))
+            {
+                var buffer = new byte[1024];
+
+                var l = inputStream.Read(buffer, 0, 1024);
+                while (l > 0)
+                {
+                    fs.Write(buffer, 0, l);
+                    l = inputStream.Read(buffer, 0, 1024);
+                }
+                fs.Flush();
+                fs.Close();
+            }
+            statuses.Add(new FilesStatus(new FileInfo(fullName)));
+        }
+
+        // Envia arquivo completo
+        private void UploadWholeFile(HttpContext context, List<FilesStatus> statuses)
+        {
+            // efetua loop na lista de arquivos
+            for (int i = 0; i < context.Request.Files.Count; i++)
+            {
+                // Pega o arquivo
+                var file = context.Request.Files[i];
+
+                // Aqui você personaliza o nome do arquivo que será salvo
+                var imgName = "__" + Path.GetFileName(file.FileName);
+
+                // Aqui você personaliza as dimensões
+                int imgHeight = 50;
+                int imgWidth = 150;
+
+                // Define o caminho completo onde o arquivo irá ser salvo (Diretório + Nome do Arquivo)
+                var fullPath = StorageRoot + imgName;
+
+                // Manipula a imagem aplicando um fundo branco e redimensonando a mesma
+                Stream imgStream = file.InputStream;
+                Bitmap newFile = ResizeImage(imgStream, imgWidth, imgHeight);
+
+                // Salva a imagem em formato Jpeg
+                newFile.Save(fullPath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                statuses.Add(new FilesStatus(imgName, file.ContentLength, fullPath));
+            }
+        }
+
+        // Retorna o JSON
+        private void WriteJsonIframeSafe(HttpContext context, List<FilesStatus> statuses)
+        {
+            context.Response.AddHeader("Vary", "Accept");
+            try
+            {
+                if (context.Request["HTTP_ACCEPT"].Contains("application/json"))
+                    context.Response.ContentType = "application/json";
+                else
+                    context.Response.ContentType = "text/plain";
+            }
+            catch
+            {
+                context.Response.ContentType = "text/plain";
+            }
+
+            var uploadedFiles = new { files = statuses.ToArray() };
+            var jsonObj = js.Serialize(uploadedFiles);
+            context.Response.Write(jsonObj);
+        }
+
+        private static bool GivenFilename(HttpContext context)
+        {
+            return !string.IsNullOrEmpty(context.Request["f"]);
+        }
+
+        private void DeliverFile(HttpContext context)
+        {
+            var filename = context.Request["f"];
+            var filePath = StorageRoot + filename;
+
+            if (File.Exists(filePath))
+            {
+                context.Response.AddHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
+                context.Response.ContentType = "application/octet-stream";
+                context.Response.ClearContent();
+                context.Response.WriteFile(filePath);
+            }
+            else
+                context.Response.StatusCode = 404;
+        }
+
+        private void ListCurrentFiles(HttpContext context)
+        {
+            var files =
+                new DirectoryInfo(StorageRoot)
+                    .GetFiles("*", SearchOption.TopDirectoryOnly)
+                    .Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden))
+                    .Select(f => new FilesStatus(f))
+                    .ToArray();
+
+            var uploadedFiles = new { files = files.ToArray() };
+            string jsonObj = js.Serialize(uploadedFiles);
+            context.Response.AddHeader("Content-Disposition", "inline; filename=\"files.json\"");
+            context.Response.Write(jsonObj);
+            context.Response.ContentType = "application/json";
+        }
+
         // Redimensionamento da imagem
-        public Bitmap ResizeImage(Stream imgStream, int? newWidth, int? newHeight)
+        private Bitmap ResizeImage(Stream imgStream, int? newWidth, int? newHeight)
         {
             Bitmap imgOut = null;
 
@@ -107,21 +254,5 @@ namespace JqueryFileUpload
 
             return imgOut;
         }
-
-        public bool IsReusable
-        {
-            get
-            {
-                return false;
-            }
-        }
-    }
-
-    public class ViewDataUploadFilesResult
-    {
-        public string thumbnail_url { get; set; }
-        public string name { get; set; }
-        public int length { get; set; }
-        public string type { get; set; }
     }
 }
